9c44cd90d1da0a822f3cfa2d3e144481984550d0
krang@VBox <jzi@inbox.ru>
2020-01-07 01:53:20 +0300

    add global variable

--- main.c
+++ main.c
@@ -21,12 +21,13 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void doSomeWork(void)
+int i = 0;
+void doSomeWork(char *name)
 {
         const int NUM_TIMES = 5;
-        for (int i = 0; i < NUM_TIMES; i++) {
+        for ( ; i < NUM_TIMES; i++) {
                 sleep(rand() % 4);
-                printf("Done pass %d\n", i);
+                printf("Done pass %d for %s\n", i, name);
         }
 }
 int main(int argc, char *argv[])
@@ -34,6 +35,7 @@ int main(int argc, char *argv[])
    printf("I am: %d\n", (int) getppid());
 
    pid_t pid = fork();
+   srand((int)pid);
    printf("fork returned: %d\n", (int) pid);
 
    if(pid < 0)
@@ -43,12 +45,13 @@ int main(int argc, char *argv[])
    if(pid == 0)
    {
            printf("I am the child with pid %d\n", (int) getppid());
-           doSomeWork();
+           doSomeWork("Child");
            printf("Child exiting...\n");
            exit(0);
    }
    // We must be the parent
    printf("I am the parent\n");
+   doSomeWork("Parent");
    wait(NULL);
    printf("Parent ending.\n");
 
